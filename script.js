8cb5eb35a973f97d4c4f7138576ab243c3e3f5e3
megamasterguido <guidozamussi@hotmail.com>
2022-12-13 05:05:57 -0300

    CSS

--- script.js
+++ script.js
@@ -22,218 +22,10 @@ class Repuesto{
     }
 }
 
-let listaRepuestos = [];
-let listaEquipos = [];
-
 function imprimir(texto){
-    if(interface == 1){
-        console.log(String(texto));
-    }
-    else if (interface == 2){
-        alert(texto);
-    }
-}
-/*
-function familiaRepuestos (){
-    let flag = 1;
-    let i = 0;
-    let repuestos = [];
-    while (flag == 1) {
-        repuestos[i] = prompt("Ingrese el NOMBRE o el CODIGO del repuesto");
-        do{
-            flag = prompt("_Ingrese 1 si desea ingresar otro repuesto.\n_Ingrese 2 si desea terminar.");
-            if(flag != 1 && flag != 2){
-                imprimir("Ingreso no valido: " + flag);
-            }
-        } while (flag != 1 && flag != 2);
-        i++;
-    }
-    return repuestos;
-}
-
-function familiaEquipos (){
-    let i = 0;
-    let equipos = [];
-    let flag = 1;
-    while (flag == 1) {
-        equipos[i] = prompt("Ingrese el NOMBRE o el CODIGO del equipo");
-        do{
-            flag = prompt("_Ingrese 1 si desea ingresar otro equipo.\n_Ingrese 2 si desea terminar.");
-            if(flag != 1 && flag != 2){
-                imprimir("Ingreso no valido.");
-            }
-        } while (flag != 1 && flag != 2);
-        i++;
-    }
-    return equipos;
-}
-*/
-function crearEquipo(){
-    this.nombre = prompt("Nombre");
-    this.marca = prompt("Marca");
-    this.fabrica = prompt("Fabrica");
+    console.log(String(texto));
     
-    let reps = prompt("Ingrese 1 si desea ingresar repuestos para este equipo o ingrese cualquier cosa en caso contrario.");
-    if(reps == 1){
-        this.repuestos = familiaRepuestos();
-    }
-    else{
-        this.repuestos = [];
-    }
-    
-    let ingresado = prompt("Si ya posee el equipo ingresado, puede agregar el codigo del mismo. Ingrese 1 para ingresar el codigo del mismo o cualquier otra cosa para no hacerlo.");
-    if (ingresado == 1){
-        this.codigo = prompt("Ingrese el codigo");
-    }
-    else{
-        this.codigo = "Sin Codigo";
-    }
-
-    return new Equipo(codigo, nombre, marca, fabrica, repuestos);
 }
-
-function crearRepuesto(){
-    let llamarIngreso = 0;
-    this.codigo = prompt("Ingrese el CODIGO del articulo");
-    this.nombre = prompt("Ingrese el NOMBRE del articulo");
-    this.esOriginal = prompt("Es original: \n1_Sí \n2_No");
-    if(esOriginal == 2){
-        llamarIngreso = prompt("Si desea indicar los repuestos originales, ingrese 1. En caso contrario, ingrese cualquier cosa.");
-        if(llamarIngreso == 1){
-            this.originales = familiaRepuestos();
-            llamarIngreso = 0;
-        }
-        else{
-            this.originales = [];
-        }
-        this.copias = [];
-    }
-    else if(esOriginal == 1){
-        llamarIngreso = prompt("Si desea indicar los repuestos copia, ingrese 1. En caso contrario, ingrese cualquier cosa.");
-        if(llamarIngreso == 1){
-            this.copias = familiaRepuestos();
-            llamarIngreso = 0;
-        }
-        else{
-            this.copias = [];
-        }
-        this.originales = [];
-    }
-    
-    llamarIngreso = prompt("Si desea indicar los repuestos alternativos, ingrese 1. En caso contrario, ingrese cualquier cosa.");
-    if(llamarIngreso == 1){
-        this.alternativos = familiaRepuestos();
-        llamarIngreso = 0;
-    }
-    else{
-        this.alternativos = [];
-    }
-    
-    llamarIngreso = prompt("Si desea indicar los repuestos para los cuales es un alternativo o puede reemplazar, ingrese 1. En caso contrario, ingrese cualquier cosa.");
-    if(llamarIngreso == 1){
-        this.alternativoDe = familiaRepuestos();
-        llamarIngreso = 0;
-    }
-    else{
-        this.alternativoDe = [];
-    }
-    
-    llamarIngreso = prompt("Si desea indicar los equipos donde va o puede ir colocado, ingrese 1. En caso contrario, ingrese cualquier cosa.");
-    if(llamarIngreso == 1){
-        this.equipos = familiaEquipos();
-        llamarIngreso = 0;
-    }
-    else{
-        this.equipos = [];
-    }
-    
-    
-    return new Repuesto(nombre, esOriginal, originales, copias, alternativos, alternativoDe, equipos, codigo);
-}
-
-function concatenarArticulos (articulos){
-
-    let resp = "";
-
-    let i = articulos.length - 1;
-
-    while(i >= 0){
-        resp = resp.concat(articulos[i]);
-        i--;
-        if(i >= 0){
-            resp = resp.concat(", ");
-        }
-    }
-    return resp;
-}
-
-function imprimirRepuesto(repuesto){
-    imprimir("Codigo: " + repuesto.codigo);
-    imprimir("Nombre: " + repuesto.nombre);
-
-    let alternativos = repuesto.alternativos.join(", ");
-    imprimir("Alternativos: " + alternativos);
-    
-    let alternativoDe = repuesto.alternativoDe.join(", ");
-    imprimir("Alternativo de: " + alternativoDe);
-    
-    let equipos = repuesto.equipos.join(", ");
-    imprimir("Equipos en los que va colocado: " + equipos);
-
-    if(repuesto.esOriginal == 1){
-        imprimir("Original: Si");
-        let copias = repuesto.copias.join(", ");
-        imprimir("Copias: " + copias);
-    }
-    else if(repuesto.esOriginal == 2){
-        imprimir("Original: No");
-        let originales = repuesto.originales.join(", ");
-        imprimir("Originales: " + originales);
-    }
-}
-
-function imprimirEquipos(equipo){
-    imprimir("Codigo: " + equipo.codigo);
-    imprimir("Nombre: " + equipo.nombre);
-    imprimir("Marca: " + equipo.marca);
-    imprimir("Fabrica: " + equipo.fabrica);
-
-    let repuestos = equipo.repuestos.join(", ");
-    imprimir("Repuestos: " + repuestos);
-}
-
-//let flag = prompt("Ingrese 1 si desea ingresar un articulo o cualquier cosa en caso contrario")
-let interface = 1; //prompt("Ingrese cómo desea continuar: \n_Ingrese 1 para continuar por consola\n_Ingrese 2 para continuar por cuadros de diálogo.");
-
-if (interface == 1) {
-    imprimir("Ha elegido proceder por consola");
-} else if (interface == 2){
-    imprimir("Ha elegido continuar por cuadros de diálogo");
-} else {
-    alert("Ingreso no válido");
-}
-
-let contadorRepuestos = 0;
-let contadorEquipos = 0;
-
-
-/*
-while(flag == 1){
-    let clase = prompt("Ingrese qué tipo de artículo desea ingresar: \n1_Equipo \n2_Repuesto");
-
-    if (clase == 2){
-        listaRepuestos.push(crearRepuesto());
-        contadorRepuestos++;
-    }
-    else if (clase == 1){
-        listaEquipos.push(crearEquipo());
-        contadorEquipos++;
-    }
-    else imprimir("Ingreso no valido")
-
-    flag = prompt("Ingrese 1 si desea continuar o cualquier otra cosa para terminar.")
-}
-*/
 
 let formulario = document.getElementById('formulario');
 
@@ -244,6 +36,7 @@ let cantidadOriginales = 0;
 let cantidadAlternativos = 0;
 let cantidadReemplazos = 0;
 let cantidadEquipos = 0;
+let cantidadReps = 0;
 
 function agregarCopias(){
     let copia = document.createElement("input");
@@ -252,13 +45,26 @@ function agregarCopias(){
     copia.id = "copia" + cantidadCopias;
     copia.type = "copia";
     copiaLabel.htmlFor = copia.id;
-    let familiaCopia = document.getElementById("familiaCopia");
+    let familiaCopias = document.getElementById("familiaCopias");
     let salto = document.createElement('br');
 
-    familiaCopia.append(copiaLabel);
-    familiaCopia.append(copia);
-    familiaCopia.append(salto);
+    let borrar = document.createElement("button");
+    borrar.type = "button";
+    borrar.innerText = "X";
+    borrar.addEventListener("click", function () {cantidadCopias--; return eliminarCuadro(copiaCuadro.id);});
+
+    let copiaCuadro = document.createElement("div");
+    copiaCuadro.id = copia.id + "Cuadro";
+
+    familiaCopias.append(copiaCuadro);
+    copiaCuadro.append(copiaLabel);
+    copiaCuadro.append(copia);
+    copiaCuadro.append(borrar);
+    copiaCuadro.append(salto);
     cantidadCopias++;
+    
+    let boton = document.getElementById("agregarCopia");
+    familiaCopias.append(boton);
 }
 
 function agregarOriginales(){
@@ -273,14 +79,27 @@ function agregarOriginales(){
     let familiaOriginales = document.getElementById("familiaOriginales");
     let salto = document.createElement('br');
 
-    familiaOriginales.append(originalLabel);
-    familiaOriginales.append(original);
-    familiaOriginales.append(salto);
+    let borrar = document.createElement("button");
+    borrar.type = "button";
+    borrar.innerText = "X";
+    borrar.addEventListener("click", function () {cantidadOriginales--; return eliminarCuadro(originalCuadro.id);});
+
+    let originalCuadro = document.createElement("div");
+    originalCuadro.id = original.id + "Cuadro";
+
+    familiaOriginales.append(originalCuadro);
+    originalCuadro.append(originalLabel);
+    originalCuadro.append(original);
+    originalCuadro.append(borrar);
+    originalCuadro.append(salto);
     cantidadOriginales++;
+    
+    let boton = document.getElementById("agregarOriginal");
+    familiaOriginales.append(boton);
 }
 
 function familiaOriginalCopias(){
-    let existeFamiliaCopia = document.getElementById("familiaCopia");
+    let existefamiliaCopias = document.getElementById("familiaCopias");
     let existeOriginal = document.getElementById("familiaOriginales");
 
     if(document.getElementById("esOriginal").value == '1'){
@@ -290,24 +109,24 @@ function familiaOriginalCopias(){
             cantidadOriginales = 0;
         }
 
-        let familiaCopia = document.createElement("fieldset");
-        familiaCopia.id = "familiaCopia";
-        familiaCopia.innerHTML = `<button type="button" id="agregarCopia">Agregar Copia</button><br>`;
-        formulario.append(familiaCopia);
+        let familiaCopias = document.createElement("fieldset");
+        familiaCopias.id = "familiaCopias";
+        familiaCopias.innerHTML = `<button type="button" id="agregarCopia">Agregar Copia</button>`;
+        formulario.append(familiaCopias);
 
         let agregarCopia = document.getElementById("agregarCopia");
         agregarCopia.addEventListener("click", agregarCopias);
     }
 
     else{
-        if(existeFamiliaCopia){
-            existeFamiliaCopia.remove();
+        if(existefamiliaCopias){
+            existefamiliaCopias.remove();
             cantidadCopias  = 0;
             cantidadOriginales = 0;
         }
         let familiaOriginales = document.createElement("fieldset");
         familiaOriginales.id = "familiaOriginales";
-        familiaOriginales.innerHTML = `<button type="button" id="agregarOriginal">Agregar Original</button><br>`;
+        familiaOriginales.innerHTML = `<button type="button" id="agregarOriginal">Agregar Original</button>`;
         formulario.append(familiaOriginales);
 
         let agregarOriginal = document.getElementById("agregarOriginal");
@@ -327,10 +146,24 @@ function agregarAlternativos(){
     let familiaAlternativos = document.getElementById("familiaAlternativos");
     let salto = document.createElement('br');
 
-    familiaAlternativos.append(alternativoLabel);
-    familiaAlternativos.append(alternativo);
-    familiaAlternativos.append(salto);
+    let borrar = document.createElement("button");
+    borrar.type = "button";
+    borrar.innerText = "X";
+    borrar.addEventListener("click", function () {cantidadAlternativos--; return eliminarCuadro(alternativoCuadro.id);});
+
+    let alternativoCuadro = document.createElement("div");
+    alternativoCuadro.id = alternativo.id + "Cuadro";
+
+
+    familiaAlternativos.append(alternativoCuadro);
+    alternativoCuadro.append(alternativoLabel);
+    alternativoCuadro.append(alternativo);
+    alternativoCuadro.append(borrar);
+    alternativoCuadro.append(salto);
     cantidadAlternativos++;
+
+    let boton = document.getElementById("agregarAlternativo");
+    familiaAlternativos.append(boton);
 }
 
 function agregarReemplazos(){
@@ -344,11 +177,23 @@ function agregarReemplazos(){
 
     let familiaReemplazos = document.getElementById("familiaReemplazos");
     let salto = document.createElement('br');
+    let borrar = document.createElement("button");
+    borrar.type = "button";
+    borrar.innerText = "X";
+    borrar.addEventListener("click", function () {cantidadReemplazos--; return eliminarCuadro(reemplazoCuadro.id);});
 
-    familiaReemplazos.append(reemplazoLabel);
-    familiaReemplazos.append(reemplazo);
-    familiaReemplazos.append(salto);
+    let reemplazoCuadro = document.createElement("div");
+    reemplazoCuadro.id = reemplazo.id + "Cuadro";
+
+    familiaReemplazos.append(reemplazoCuadro);
+    reemplazoCuadro.append(reemplazoLabel);
+    reemplazoCuadro.append(reemplazo);
+    reemplazoCuadro.append(borrar);
+    reemplazoCuadro.append(salto);
     cantidadReemplazos++;
+    
+    let boton = document.getElementById("agregarReemplazo");
+    familiaReemplazos.append(boton);
 }
 
 function agregarEquipos(){
@@ -363,32 +208,84 @@ function agregarEquipos(){
     let familiaEquipos = document.getElementById("familiaEquipos");
     let salto = document.createElement('br');
 
-    familiaEquipos.append(equipoLabel);
-    familiaEquipos.append(equipo);
-    familiaEquipos.append(salto);
+    let borrar = document.createElement("button");
+    borrar.type = "button";
+    borrar.innerText = "X";
+    borrar.addEventListener("click", function () {cantidadEquipos--; return eliminarCuadro(equipoCuadro.id);});
+
+    let equipoCuadro = document.createElement("div");
+    equipoCuadro.id = equipo.id + "Cuadro";
+
+    familiaEquipos.append(equipoCuadro);
+    equipoCuadro.append(equipoLabel);
+    equipoCuadro.append(equipo);
+    equipoCuadro.append(borrar);
+    equipoCuadro.append(salto);
     cantidadEquipos++;
+    
+    let boton = document.getElementById("agregarEquipo");
+    familiaEquipos.append(boton);
+}
+
+function agregarReps(){
+    let rep = document.createElement("input");
+    let repLabel = document.createElement("label");
+    repLabel.innerText = "Introduzca el codigo del repuesto";
+    rep.id = "rep" + cantidadReps;
+    rep.type = "rep";
+    repLabel.htmlFor = rep.id;
+    
+    let familiaReps = document.getElementById("familiaReps");
+    let salto = document.createElement("br");
+    
+    let borrar = document.createElement("button");
+    borrar.type = "button";
+    borrar.innerText = "X";
+    borrar.addEventListener("click", function () {cantidadReps--; return eliminarCuadro(repCuadro.id);});
+
+    let repCuadro = document.createElement("div");
+    repCuadro.id = rep.id + "Cuadro";
+
+    familiaReps.append(repCuadro);
+    repCuadro.append(repLabel);
+    repCuadro.append(rep);
+    repCuadro.append(borrar);
+    repCuadro.append(salto);
+    cantidadReps++;
+    
+    let boton = document.getElementById("agregarRep");
+    familiaReps.append(boton);
+}
+
+function eliminarCuadro(cuadro){
+    let box = document.getElementById(cuadro);
+    box.remove();
 }
 
 function generarFormulario(){
     if (tipo.value == "Repuesto"){
-        formulario.innerHTML = `<label for="nombre">Nombre del repuesto</label>
-        <input type="nombre" id="nombre"><br>
-        <label for="fabricante">Fabricante del repuesto</label>
-        <input type="fabricante" id="fabricante"><br>
-        <label for="codigo">Codigo del repuesto</label>
-        <input type="codigo" id="codigo"><br>
-        <label for="esOriginal"> Es original </label>
+        formulario.innerHTML = `<div id="cabeza"><div class="bloqueForm"><label for="nombre">Nombre del repuesto</label>
+        <input type="nombre" id="nombre"></div>
+        <div class="bloqueForm"><label for="fabricante">Fabricante del repuesto</label>
+        <input type="fabricante" id="fabricante"></div>
+        <div class="bloqueForm"><label for="codigo">Codigo del repuesto</label>
+        <input type="codigo" id="codigo"></div>
+        <div class="bloqueForm"><label for="esOriginal"> Es original </label>
         <select name="esOriginal" id="esOriginal" form="formulario">
             <option value=1> Si </option>
             <option value=0> No </option>
-        </select><br>`;
+        </select></div></div>`;
+
+        cantidadAlternativos = 0;
+        cantidadReemplazos = 0;
+        cantidadEquipos = 0;
         
         familiaOriginalCopias();
         document.getElementById("esOriginal").addEventListener("change", familiaOriginalCopias);
 
         let familiaAlternativos = document.createElement("fieldset");
         familiaAlternativos.id = "familiaAlternativos";
-        familiaAlternativos.innerHTML = `<button type="button" id="agregarAlternativo">Agregar alternativo</button><br>`;
+        familiaAlternativos.innerHTML = `<button type="button" id="agregarAlternativo">Agregar alternativo</button>`;
         formulario.append(familiaAlternativos);
 
         let agregarAlternativo = document.getElementById("agregarAlternativo");
@@ -397,7 +294,7 @@ function generarFormulario(){
         
         let familiaReemplazos = document.createElement("fieldset");
         familiaReemplazos.id = "familiaReemplazos";
-        familiaReemplazos.innerHTML = `<button type="button" id="agregarReemplazo">Agregar reemplazo</button><br>`;
+        familiaReemplazos.innerHTML = `<button type="button" id="agregarReemplazo">Agregar reemplazo</button>`;
         formulario.append(familiaReemplazos);
 
         let agregarReemplazo = document.getElementById("agregarReemplazo");
@@ -405,7 +302,7 @@ function generarFormulario(){
         
         let familiaEquipos = document.createElement("fieldset");
         familiaEquipos.id = "familiaEquipos";
-        familiaEquipos.innerHTML = `<button type="button" id="agregarEquipo">Agregar equipo</button><br>`;
+        familiaEquipos.innerHTML = `<button type="button" id="agregarEquipo">Agregar equipo</button>`;
         formulario.append(familiaEquipos);
 
         let agregarEquipo = document.getElementById("agregarEquipo");
@@ -413,27 +310,27 @@ function generarFormulario(){
 
     }
     else {
-        formulario.innerHTML = `<label for="nombre">Nombre del electrodomestico</label><input type="nombre" id="nombre">`;
+        formulario.innerHTML = `<div class="bloqueForm"><label for="nombre">Codigo (si lo posee)</label>
+        <input type="codigo" id="codigo"></div>
+        <div class="bloqueForm"><label for="nombre">Nombre </label>
+        <input type="nombre" id="nombre"></div>
+        <div class="bloqueForm"><label for="marca">Marca</label>
+        <input type="marca" id="marca"></div>
+        <div class="bloqueForm"><label for="fabrica">Fabrica</label>
+        <input type="fabrica" id="fabrica"></div>
+        `;
+        
+        cantidadReps = 0;
+
+        let familiaReps = document.createElement("fieldset");
+        familiaReps.id = "familiaReps";
+        familiaReps.innerHTML = `<button type="button" id="agregarRep">Agregar repuesto</button>`;
+        formulario.append(familiaReps);
+
+        let agregarRep = document.getElementById("agregarRep");
+        agregarRep.addEventListener("click", agregarReps);
     }
 }
 
 generarFormulario();
 tipo.addEventListener("change", generarFormulario);
-
-/*
-if(contadorRepuestos == 0){
-    imprimir("No se han ingresado repuestos.");
-}
-else{
-    imprimir("Se han ingresado " + contadorRepuestos + " repuestos:");
-    listaRepuestos.forEach(repuesto => imprimirRepuesto(repuesto));
-}
-
-if(contadorEquipos == 0){
-    imprimir("No se han ingresado equipos.");
-}
-else{
-    imprimir("Se han ingresado " + contadorEquipos + " equipos:");
-    listaEquipos.forEach(equipo => imprimirEquipos(equipo));
-}
-*/
